8026c2fc58679ff0ef53c9e798619928177612b7
dkyuh <daniel.k.hopfner@gmail.com>
2023-10-26 00:25:20 +0200

    update gh Thu Oct 26 00:25:20 CEST 2023

---
+++ _private_update_pages.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+git checkout gh-pages
+
+# get the directory from the main branch
+git checkout main -- cmf_jekyll
+
+cp -r cmf_jekyll/_site/* .
+
+git add .
+git reset -- cmf_jekyll
+git commit -m "update gh $(date)"
+git push origin gh-pages
+
+git checkout main
